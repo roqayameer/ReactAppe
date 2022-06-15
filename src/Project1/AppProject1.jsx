5bb93024ab46db49c79d28586f7ba6376bb62261
Roqaya_Ameer <roqayaameer312@gmail>
2022-06-15 17:35:14 +0300

    AppProject1

---
+++ src/Project1/AppProject1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+export default function AppProject1() {
+  return (
+    <>
+      {" "}
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
