f8f3bbc5198e6b22e81fe6e65b1fd069441812c2
Roqaya_Ameer <roqayaameer312@gmail>
2022-06-23 18:44:55 +0300

    ;l,;',;'

---
+++ src/ReactProjectApi/Api_Project.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Apifetch.css";
+export default function Api_Project() {
+  return (
+    <>
+      <h1>our Project</h1>
+      <div className="app">
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <h1>Prodect number List her:</h1>
+        <div className="prodect">
+          <h1>title</h1>
+          <img src="" alt="" />
+          <p>description</p>
+        </div>
+      </div>
+    </>
+  );
+}
